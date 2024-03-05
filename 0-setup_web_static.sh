8c3c2a5e1caa9ecb14712e1b14f170dc42acc5e9
khayo254 <khayo1998@gmail.com>
2024-03-05 23:09:44 +0300

    preparation bashscript

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,27 @@
 #!/usr/bin/env bash
-# Check if Nginx is installed, install it if not found
-if ! command -v nginx >/dev/null 2>&1; then
-    sudo apt-get update && sudo apt-get install -y nginx
-fi
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Ensure necessary directories exist
-ensure_directory_exists() {
-    local directory=$1
-    if [[ ! -d "$directory" ]]; then
-        mkdir -p "$directory"
-    fi
-}
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-ensure_directory_exists "/data"
-ensure_directory_exists "/data/web_static"
-ensure_directory_exists "/data/web_static/releases"
-ensure_directory_exists "/data/web_static/shared"
-ensure_directory_exists "/data/web_static/releases/test"
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Create a dummy HTML file in the test directory
-echo "<html><body>Hello World!</body></html>" > /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /data/
 
-# Create a symbolic link to the current release directory
-if [[ -L "/data/web_static/current" ]]; then
-    rm -f "/data/web_static/current"
-fi
-ln -s "/data/web_static/releases/test" "/data/web_static/current"
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Assign ownership of the data directory to the ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data
-
-# Update Nginx configuration to serve content from /data/web_static/current/ to hbnb_static
-sudo sed -i 's|server {|server {\n\tlisten 80;\n\tserver_name mydomainname.tech;\n\troot /data/web_static/current;\n}|g' /etc/nginx/sites-available/default
-
-# Restart Nginx service
 sudo service nginx restart
